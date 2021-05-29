d06b6c9c3a09169a20d37d5c51d0eae3053b962f
msaaddev <mrsaadirfan@gmail.com>
2021-05-29 15:42:46 +0500

    📦 NEW: SEO

---
+++ components/SEO.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+const SEO = () => {
+	return (
+		<Head>
+			<title>GeoWeather – One stop to get all weather information</title>
+			<meta charset="UTF-8" />
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta
+				name="description"
+				content="A weather web app that you can use to get weather information "
+			/>
+			<link rel="icon" href="/sun.png" />
+			<meta name="keywords" content="weather, cloudy, thunder-storm" />
+		</Head>
+	);
+};
+
+export default SEO;
