dd2e4d54c5ddc8278af8aa13225bddb5772fc909
msaaddev <mrsaadirfan@gmail.com>
2021-06-03 23:35:55 +0500

    📦 NEW: font

--- components/SEO.js
+++ components/SEO.js
@@ -15,6 +15,12 @@ const SEO = () => {
 			/>
 			<link rel="icon" href="/sun.png" />
 			<meta name="keywords" content="weather, cloudy, thunder-storm" />
+
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link
+				href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500;1,700;1,900&display=swap"
+				rel="stylesheet"
+			/>
 		</Head>
 	);
 };
