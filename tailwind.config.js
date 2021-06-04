180215903bb8c44e3c956648653b159af61eaad1
msaaddev <mrsaadirfan@gmail.com>
2021-06-04 15:13:06 +0500

    📦 NEW: screen configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
 	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		screens: {
+			'2xl': { max: '1535px' },
+			xl: { max: '1279px' },
+			lg: { max: '1023px' },
+			md: { max: '767px' },
+			sm: { max: '639px' }
+		},
 		extend: {
 			height: {
 				'10v': '10vh',
